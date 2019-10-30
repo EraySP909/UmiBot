c4dfa977d3294264bbe1872541f11dc0b6efb116
EraySP909 <erayunsurorluu@gmail.com>
2019-10-30 11:45:47 +0100

    Removed an fixed typos

--- bot.js
+++ bot.js
@@ -7,7 +7,6 @@ const botID = botSettings.botID;
 const fortniteClient = require("fortnite");
 const fortnite = new fortniteClient("1682c643-dcfe-4454-a497-c32ab2264a62");
 const attachment = new Discord.Attachment;
-const randomumi = ['./images/umi1.png','./images/umi2.png','./images/umi3.png',];
 const museMember = ['Maki','Rin','Hanayo','Honoka','Kotori','Nozomi','Eli','Nico','Myself???',];
 const prefix = "u!";
 const niconiconii = client.emojis.get("500213244749938688");
@@ -38,21 +37,17 @@ client.on('message', message => {
   if (command === "help") {
       
   const embed = new Discord.RichEmbed()
-  .addField("ping", "Pings Umiboi", true)
+  .addField("ping", "Pings Umibot", true)
   .addField("ap", "Activity Playing", true)
   .addField("al", "Activity Listening", true)
   .addField("aw", "Activity Watching", true)
   .addField("as", "Activity Streaming", true)
   .addField("say", "Say", true)
   .addField("poll", "Poll with Y/N", true)
-  .addField("randomumi", "Random Umi/Mimorin", true)
-  .addField("meme","Random Meme", true)
   .addField("mistakes", "MISTAKES", true)
   .addField("eval", "Eval", true)
-  .addField("sqaaakoi", "HAHA YES", true)
   .addField("die-yourself", "Kill myself lmao", true)
   .addField("Soon:tm:", ":)", true)
-  .addField("hentai:", "No need to explain do I?", true)
   message.channel.send(embed);
   }
   // Activity Playing	  
@@ -115,18 +110,6 @@ client.on('message', message => {
       message.channel.send(`Poll: \n${pollMessage}`)
       .then(message => {message.react("🇾"), message.react("🇳")});
   }
-  // Random Umi picture
-  if(command === `randomumi`) {
-       number = 611;
-       imageNumber = Math.floor(Math.random() * (number - 1 + 1)) + 1;
-       message.channel.send({files:["./images/" + imageNumber + ".png"]});
-  }
-  // Random Meme  
-  if(command === `meme`){
-    number = 789;
-    imageNumber = Math.floor(Math.random() * (number - 1 + 1)) +1;
-    message.channel.send({files:["./memes/" + imageNumber + ".png"]})
-  }
   // !! EVAL !! USE WITH CAUTION  
    if (message.content.startsWith(prefix + "eval")) {
     if(message.author.id !== ownerID) return;
@@ -154,25 +137,11 @@ client.on('message', message => {
      .setColor("#3636F") //#34363C
      message.channel.send(embed)
   }
-  if(command === "sqaaakoi"){
-     message.channel.send("haha small love, no homo");
-  }
   if(command === "mistakes") {
       const embed = new Discord.RichEmbed()
       .addField("ufn", "Obvious Mistype", true)
       .addField("This bot", "haha yes", true)
-      .addField("u!sqaakoi", "BIG MISTYPE", trxue)
-      .addField("Hentai command~", "u!hentai", true)
-//  message.channel.send("ufn")
-//  message.channel.send("this bot's existence")
-//  message.channel.send("u!sqaakoi")
-//  message.channel.send("making a hentai command")
       message.channel.send(embed);
-  }
-  if(command === "hentai") {
-    number = 88;
-    imageNumber = Math.floor(Math.random() * (number - 1 + 1)) +1;
-    message.channel.send({files:["./hentaiimages/" + imageNumber + ".png"]})
   }
 if (command === "listemojis") {
    const emojiList = message.guild.emojis.map((e, x) => (x + ' = ' + e) + ' | ' +e.name).join('\n');
@@ -192,45 +161,3 @@ let seconds = totalSeconds % 60;
 });
 
 client.login(botSettings.token);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// https://discordapp.com/oauth2/authorize?client_id=491709582050328576&permissions=8&scope=bot
-//  _____  _                                             _             _
-// |  __ \| |                                           | |           | |
-// | |__) | | ___  __ _ ___  ___     _ __   ___      ___| |_ ___  __ _| |
-// |  ___/| |/ _ \/ _` / __|/ _ \   | '_ \ / _ \    / __| __/ _ \/ _` | |
-// | |    | |  __/ (_| \__ \  __/   | | | | (_) |   \__ \ ||  __/ (_| | |
-// |_|    |_|\___|\__,_|___/\___|   |_| |_|\___/    |___/\__\___|\__,_|_|
-//     __   
-//  _  \ \  
-// (_)  | | 
-//      | | 
-//  _   | | 
-// (_)  | | 
-//     /_/ 
-
-//https://images4.alphacoders.com/376/thumb-1920-37606.jpg
